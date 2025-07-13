c206055048939dc07eed0ccdb2f4112c41a71ef7
dominicjosephh <dominicdd4@aol.com>
2025-07-13 15:04:08 -0500

    Update music.py

--- music.py
+++ music.py
@@ -1,1 +1,8 @@
-
+1 file changed, 20 insertions(+)
+ create mode 100644 speech_service.py
+root@JunoLive:~/junopresence-goldilocks# python3 main.py
+Traceback (most recent call last):
+  File "/root/junopresence-goldilocks/main.py", line 5, in <module>
+    from music import handle_music_command
+ModuleNotFoundError: No module named 'music'
+root@JunoLive:~/junopresence-goldilocks# 
